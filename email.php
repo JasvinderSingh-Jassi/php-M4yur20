1f3e60e04d9e619a32b53ddda8c4c336bdf052a4
Mayur Agarwal <mayuragarwal929@gmail.com>
2021-05-25 16:23:57 +0530

    DRY email errors coming will do after exam

--- email.php
+++ email.php
@@ -109,7 +109,7 @@ foreach($emails as $email){
         </div>
         </div>
         <div style="margin-left:13px;">If you would prefer not to receive comics in future from us
-        <a href="https://'.$_SERVER['SERVER_NAME'].'/unsubscribe.php?email='.$email.'" style="color:red">unsubscribe here.</a></div>
+        <a href="https://calm-journey-40539.herokuapp.com/unsubscribe.php?email='.$email.'" style="color:red">unsubscribe here.</a></div>
         </body>
         </html>
         
