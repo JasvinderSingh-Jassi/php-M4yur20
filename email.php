932869b11e6e41297ad8b4d97eb235d8ff6cdf93
Mayur Agarwal <mayuragarwal929@gmail.com>
2021-05-26 01:31:40 +0530

    all done

--- email.php
+++ email.php
@@ -10,11 +10,10 @@ while ($row = $result->fetch_assoc()) {
     array_push($hashes,$row['hash']);
 }
 
-function getSiteOG( $url, $specificTags=0 ){
+function getSiteOG($url, $specificTags=0){
     $doc = new DOMDocument();
     @$doc->loadHTML(file_get_contents($url));
     $res['title'] = $doc->getElementsByTagName('title')->item(0)->nodeValue;
-
     foreach ($doc->getElementsByTagName('meta') as $m){
         $tag = $m->getAttribute('name') ?: $m->getAttribute('property');
         if(in_array($tag,['description','keywords']) || strpos($tag,'og:')===0) $res[str_replace('og:','',$tag)] = $m->getAttribute('content');
--- emailverify.php
+++ emailverify.php
@@ -227,11 +227,10 @@ if($row['is_active']=='0'){
     }
 }
 if($uresult and !$isalrdyact and !$hash_error){ 
-  function getSiteOG( $url, $specificTags=0 ){
+  function getSiteOG($url, $specificTags=0){
     $doc = new DOMDocument();
     @$doc->loadHTML(file_get_contents($url));
     $res['title'] = $doc->getElementsByTagName('title')->item(0)->nodeValue;
-
     foreach ($doc->getElementsByTagName('meta') as $m){
         $tag = $m->getAttribute('name') ?: $m->getAttribute('property');
         if(in_array($tag,['description','keywords']) || strpos($tag,'og:')===0) $res[str_replace('og:','',$tag)] = $m->getAttribute('content');
